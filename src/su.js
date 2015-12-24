65a2ea4d362aaf6bef59f5bcf6786d3f95d547f0
Eugene Girshov <eugene@solu.co>
2015-12-24 23:36:13 +0200

    add reconnect support

--- src/su.js
+++ src/su.js
@@ -36,7 +36,71 @@ function connect (stream_url, options, callback) {
     if (!adaptor) {
         throw new Error('protocol unknown: '+u.protocol);
     }
-    return adaptor.connect(u, options, callback);
+
+    if (options && options.reconnect) {
+        var connector = {
+            stream: null,
+            connecting: false,
+            enabled: true,
+            retries: 0,
+            minDelay: options.reconnect.minDelay || 1000,
+            currentDelay: 0,
+            callback: function (err, stream) {
+                connector.connecting = false;
+                if (!err) {
+                    connector.stream = stream;
+                    stream.once('close', function () {
+                        connector.stream = null;
+                        connector._connect();
+                    });
+                    callback(null, stream);
+                } else {
+                    connector.retries += 1;
+                    if (options.reconnect.maxRetries &&
+                        connector.retries >= options.reconnect.maxRetries) {
+                       connector.enabled = false;
+                       callback(err, null);
+                    } else {
+                        connector.reconnect();
+                    }
+                }
+            },
+            connect: function () {
+                connector.enabled = true;
+                connector.retries = 0;
+                connector.currentDelay = connector.minDelay;
+                return connector._connect();
+            },
+            _connect: function () {
+                if (!connector.enabled)
+                    return;
+                if (connector.stream) {
+                    connector.stream.end();
+                } else if (!connector.connecting) {
+                    connector.connecting = true;
+                    adaptor.connect(u, options, connector.callback);
+                } else {
+                    // already in progress
+                }
+            },
+            reconnect: function () {
+                var timeout = Math.random() * (connector.currentDelay - connector.minDelay) +
+                              connector.minDelay;
+                connector.currentDelay *= 2;
+                setTimeout(function () {
+                    connector._connect();
+                }, timeout);
+            },
+            disable: function () {
+                connector.enabled = false;
+                return connector;
+            },
+        };
+        connector.connect();
+        return connector;
+    } else {
+        return adaptor.connect(u, options, callback);
+    }
 }
 
 function register (protocol, listen_handler, connect_handler) {
